e3ec4edf8b6a8c7bad7b8a6732eba97c4480924e
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 18:17:06 -0500

    add tests for day 2 c#

---
+++ tests/csharp/Y2025/Day02Tests.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode.Tests.Y2025;
+
+public sealed class Day02Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2025, 2);
+
+    private const string Sample = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("1227775554", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("4174379265", p2);
+    }
+}
