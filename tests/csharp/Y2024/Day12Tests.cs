5540f889bc4b0f52eee3aec66422a0280c3470af
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 21:57:20 -0500

    2024 day 12 c#

---
+++ tests/csharp/Y2024/Day12Tests.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode.Tests.Y2024;
+
+public class Day12Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2024, 12);
+
+    private const string Sample = """
+    RRRRIICCFF
+    RRRRIICCCF
+    VVRRRCCFFF
+    VVRCCCJFFF
+    VVVVCJJCFE
+    VVIVCCJJEE
+    VVIIICJJEE
+    MIIIIIJJEE
+    MIIISIJEEE
+    MMMISSJEEE
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("1930", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("1206", p2);
+    }
+}
