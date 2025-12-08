0f1ab8767e6de03d2a876b2b710d183fb802676b
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 22:01:40 -0500

    2024 day 13 c#

---
+++ tests/csharp/Y2024/Day13Tests.cs
@@ -0,0 +1,31 @@
+namespace AdventOfCode.Tests.Y2024;
+
+public class Day13Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2024, 13);
+
+    private const string Sample = """
+    Button A: X+94, Y+34
+    Button B: X+22, Y+67
+    Prize: X=8400, Y=5400
+
+    Button A: X+26, Y+66
+    Button B: X+67, Y+21
+    Prize: X=12748, Y=12176
+
+    Button A: X+17, Y+86
+    Button B: X+84, Y+37
+    Prize: X=7870, Y=6450
+
+    Button A: X+69, Y+23
+    Button B: X+27, Y+71
+    Prize: X=18641, Y=10279
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("480", p1);
+    }
+}
