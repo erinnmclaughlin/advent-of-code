ca0e0d7e9a826888dada033fe84acab0cacba85f
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 19:49:23 -0500

    2025 day 4 c#

---
+++ csharp/Y2025/Day04.cs
@@ -0,0 +1,67 @@
+namespace AdventOfCode.Y2025;
+
+public sealed class Day04 : IAdventDay
+{
+    public int Year => 2025;
+    public int Day => 4;
+
+    public AdventDaySolution Solve(string input)
+    {
+        var lines = input.Split(
+            ['\r', '\n'],
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        var grid = PaperRollGrid.Parse(lines);
+
+        var totalCount = 0;
+        int initialRemovedCount = grid.RemoveRemovableRolls();
+        var lastRemovedCount = initialRemovedCount;
+
+        while (lastRemovedCount > 0)
+        {
+            totalCount += lastRemovedCount;
+            lastRemovedCount = grid.RemoveRemovableRolls();
+        }
+
+        return (initialRemovedCount, totalCount);
+    }
+}
+
+file sealed class PaperRollGrid(List<PaperRoll> rolls)
+{
+    private readonly HashSet<PaperRoll> _rolls = [.. rolls];
+
+    public bool CanRemove(PaperRoll roll)
+    {
+        return _rolls.Where(other => other.IsAdjacentTo(roll)).Take(4).Count() < 4;
+    }
+
+    public int RemoveRemovableRolls()
+    {
+        var removable = _rolls.AsParallel().Where(CanRemove).ToHashSet();
+        return _rolls.RemoveWhere(removable.Contains);
+    }
+
+    public static PaperRollGrid Parse(string[] lines)
+    {
+        var rolls = new List<PaperRoll>();
+
+        for (var row = 0; row < lines.Length; row++)
+            for (var col = 0; col < lines[row].Length; col++)
+                if (lines[row][col] == '@')
+                    rolls.Add(new PaperRoll(col, row));
+
+        return new PaperRollGrid(rolls);
+    }
+}
+
+file sealed record PaperRoll(int Col, int Row)
+{
+    public bool IsAdjacentTo(PaperRoll other) =>
+        other != this &&
+        other.Col >= Col - 1 &&
+        other.Col <= Col + 1 &&
+        other.Row >= Row - 1 &&
+        other.Row <= Row + 1;
+}
---
+++ tests/csharp/Y2025/Day04Tests.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode.Tests.Y2025;
+
+public class Day04Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2025, 4);
+
+    private const string Sample = """
+    ..@@.@@@@.
+    @@@.@.@.@@
+    @@@@@.@.@@
+    @.@@@@..@.
+    @@.@@@@.@@
+    .@@@@@@@.@
+    .@.@.@.@@@
+    @.@@@.@@@@
+    .@@@@@@@@.
+    @.@.@@@.@.
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("13", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("43", p2);
+    }
+}
