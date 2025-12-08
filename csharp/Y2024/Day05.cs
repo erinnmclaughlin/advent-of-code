6bde92a383161ac5c6d97abface350de9b368e7f
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 21:22:59 -0500

    2024 day 5 c#

---
+++ csharp/Y2024/Day05.cs
@@ -0,0 +1,40 @@
+namespace AdventOfCode.Y2024;
+
+public sealed class Day05 : IAdventDay
+{
+    public int Year => 2024;
+    public int Day => 5;
+
+    public AdventDaySolution Solve(string input)
+    {
+        var (numbers, comparer) = ParseInput(input);
+
+        var part1 = numbers
+            .Select(x => x.Split(','))
+            .Where(x => IsOrdered(x, comparer))
+            .Sum(x => int.Parse(x[x.Length / 2]));
+
+        var part2 = numbers
+            .Select(x => x.Split(','))
+            .Where(x => !IsOrdered(x, comparer))
+            .Select(x => x.Order(comparer).ToArray())
+            .Sum(x => int.Parse(x[x.Length / 2]));
+
+        return (part1, part2);
+    }
+    
+    private static (string[], Comparer<string>) ParseInput(string input)
+    {
+        var lines = InputHelper.GetLines(input);
+        var splitIndex = lines.IndexOf("");
+        var numbers = lines[(splitIndex + 1)..].ToArray();
+        var comparer = CreateComparer(lines[..splitIndex].ToArray());
+        return (numbers, comparer);
+    }
+
+    private static bool IsOrdered(string[] items, Comparer<string> comparer)
+        => items.SequenceEqual(items.Order(comparer));
+
+    private static Comparer<string> CreateComparer(string[] rules)
+        => Comparer<string>.Create((x, y) => rules.AsSpan().Contains($"{x}|{y}") ? - 1 : 1);
+}
---
+++ tests/csharp/Y2024/Day05Tests.cs
@@ -0,0 +1,51 @@
+namespace AdventOfCode.Tests.Y2024;
+
+public class Day05Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2024, 5);
+
+    private const string Sample = """
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("143", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("123", p2);
+    }
+}
