d32762dfe897f5e8467bbd8863d509acd434fc4b
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 20:08:31 -0500

    2025 day 6 c#

---
+++ csharp/Y2025/Day06.cs
@@ -0,0 +1,83 @@
+namespace AdventOfCode.Y2025;
+
+public sealed class Day06 : IAdventDay
+{
+    public int Year => 2025;
+    public int Day => 6;
+
+    public AdventDaySolution Solve(string input)
+    {
+        var lines = input.Split(['\n', '\r'], StringSplitOptions.RemoveEmptyEntries);
+        var operators = lines[^1].Where(c => c is not ' ').ToArray();
+
+        var part1 = SolvePartOne(lines, operators);
+        var part2 = SolvePartTwo(lines, operators);
+
+        return (part1, part2);
+    }
+
+    private static long SolvePartOne(string[] lines, char[] operators)
+    {
+        var sum = 0L;
+
+        for (var i = 0; i < operators.Length; i++)
+        {
+            var op = operators[i];
+            long? columnResult = null;
+
+            foreach (var line in lines[..^1])
+            {
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var number = int.Parse(parts[i].Trim());
+                columnResult = DoOperation(columnResult, number, op);
+            }
+
+            sum += (columnResult ?? 0);
+        }
+
+        return sum;
+    }
+
+    private static long SolvePartTwo(string[] lines, char[] operators)
+    {
+        var sum = 0L;
+        var stack = new Stack<char>(operators);
+
+        long? columnResult = null;
+        var op = stack.Pop();
+
+        for (var i = lines[0].Length - 1; i >= 0; i--)
+        {
+            var sb = new System.Text.StringBuilder();
+
+            foreach (var line in lines[..^1])
+            {
+                if (int.TryParse(line[i].ToString(), out var number))
+                {
+                    sb.Append(number);
+                }
+            }
+
+            if (sb.Length == 0)
+            {
+                sum += columnResult ?? 0L;
+                columnResult = null;
+                op = stack.Pop();
+            }
+            else
+            {
+                var parsedNumber = long.Parse(sb.ToString());
+                columnResult = DoOperation(columnResult, parsedNumber, op);
+            }
+        }
+
+        return sum + columnResult ?? 0L;
+    }
+
+    private static long DoOperation(long? num1, long num2, char op) => num1 is null ? num2 : op switch
+    {
+        '+' => num1.Value + num2,
+        '*' => num1.Value * num2,
+        _ => throw new NotSupportedException()
+    };
+}
---
+++ tests/csharp/Y2025/Day06Tests.cs
@@ -0,0 +1,27 @@
+namespace AdventOfCode.Tests.Y2025;
+
+public class Day06Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2025, 6);
+
+    private const string Sample = """
+    123 328  51 64 
+     45 64  387 23 
+      6 98  215 314
+    *   +   *   +  
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("4277556", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("3263827", p2);
+    }
+}
