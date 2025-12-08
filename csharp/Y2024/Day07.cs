8b6f3f773838aa69b941a009e4365cb5eeccaa52
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 21:29:43 -0500

    2024 day 7 c#

---
+++ csharp/Y2024/Day07.cs
@@ -0,0 +1,72 @@
+namespace AdventOfCode.Y2024;
+
+public sealed class Day07 : IAdventDay
+{
+    public int Year => 2024;
+    public int Day => 7;
+
+    public AdventDaySolution Solve(string input)
+    {
+        var lines = ParseInput(input);
+
+        var part1 = lines.Sum(line =>
+        {
+            var (expected, numbers) = ParseLine(line);
+            return GetPossibleOutcomes(expected, numbers, allowConcat: false).FirstOrDefault(o => o == expected);
+        });
+
+        var part2 = lines.Sum(line =>
+        {
+            var (expected, numbers) = ParseLine(line);
+            return GetPossibleOutcomes(expected, numbers, allowConcat: true).FirstOrDefault(o => o == expected);
+        });
+
+        return (part1, part2);
+    }
+    
+    private static ParallelQuery<string> ParseInput(string input) => InputHelper.GetLines(input).AsParallel();
+
+    private static (long, long[]) ParseLine(string line)
+    {
+        var parts = line.Split(": ");
+        var expected = long.Parse(parts[0]);
+        var numbers = parts[1].Split(' ').Select(long.Parse).ToArray();
+        return (expected, numbers);
+    }
+
+    private static IEnumerable<long> GetPossibleOutcomes(long maxValue, long[] values, bool allowConcat)
+    {
+        // we've exceeded the max value there's no need to keep going
+        if (values[0] > maxValue)
+            yield break;
+
+        if (values.Length == 1)
+        {
+            yield return values[0];
+            yield break;
+        }
+
+        var (current, next) = (values[0], values[1]);
+        values = values[1..];
+        
+        // doing this in the order that's most likely to grow faster (so we will break out sooner)
+        
+        // multiply
+        values[0] = current * next;
+        foreach (var r in GetPossibleOutcomes(maxValue, values, allowConcat)) 
+            yield return r;
+        
+        // concat
+        if (allowConcat)
+        {
+            values[0] = long.Parse($"{current}{next}");
+            foreach (var r in GetPossibleOutcomes(maxValue, values, allowConcat)) 
+                yield return r;
+        }
+        
+        // add
+        values[0] = current + next;
+        foreach (var r in GetPossibleOutcomes(maxValue, values, allowConcat)) 
+            yield return r;
+    }
+}
---
+++ tests/csharp/Y2024/Day07Tests.cs
@@ -0,0 +1,32 @@
+namespace AdventOfCode.Tests.Y2024;
+
+public class Day07Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2024, 7);
+
+    private const string Sample = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("3749", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("11387", p2);
+    }
+}
