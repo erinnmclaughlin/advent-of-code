ff700903fe60500bc74844b25bb49bd8d490e99c
Erin McLaughlin <sa-em5mb@myuva.onmicrosoft.com>
2025-12-08 12:31:36 -0500

    some more cleanup

--- csharp/Y2025/Day08.cs
+++ csharp/Y2025/Day08.cs
@@ -19,49 +19,52 @@ public sealed class Day08() : AdventDay(2025, 8)
     
     public sealed class JunctionBox
     {
-        public Circuit Circuit { get; set; }
-        public (int X, int Y, int Z) Position { get; }
+        public Circuit Circuit { get; set; } = new();
+        public (long X, long Y, long Z) Position { get; }
         
-        public JunctionBox(int x, int y, int z)
+        public JunctionBox(long x, long y, long z)
         {
             Position = (x, y, z);
-            Circuit = new Circuit();
             Circuit.Attach(this);
         }
 
         public void ConnectTo(JunctionBox other)
         {
-            Circuit.Junctions.ToList().ForEach(other.Circuit.Attach);
+            var junctionsToTransfer = Circuit.Junctions.ToList();
+            junctionsToTransfer.ForEach(other.Circuit.Attach);
         }
+
+        public float GetDistanceTo(JunctionBox other) => (AsVector3() - other.AsVector3()).Length();
         
-        public float GetDistanceTo(JunctionBox other)
-        {
-            return Vector3.Distance(GetVector3(), other.GetVector3());
-        }
-        
-        public Vector3 GetVector3()
-        {
-            return new Vector3(Position.X, Position.Y, Position.Z);
-        }
+        private Vector3 AsVector3() => new(Position.X, Position.Y, Position.Z);
     }
-    
-    public override AdventDaySolution Solve(string input)
+
+    public override AdventDaySolution Solve(string input) => Solve(input, 1000);
+
+    public static AdventDaySolution Solve(string input, int partOneConnectionCount)
     {
-        var junctionBoxes = ParseInput(input).ToArray();
-        var circuits = junctionBoxes.Select(x => x.Circuit).ToHashSet();
-        var (processedCount, part1, part2) = (0, 0L, 0L);
+        var junctionBoxes = ParseInput(input);
+        var circuits = junctionBoxes.Select(x => x.Circuit).ToList();
         
-        foreach (var (p1, p2) in EnumerateOrderedPairs(junctionBoxes))
+        var (part1, part2) = (0L, 0L);
+        
+        foreach (var (p1, p2) in junctionBoxes.EnumerateOrderedPairs())
         {
             p1.ConnectTo(p2);
-            circuits.RemoveWhere(c => c.Junctions.Count == 0);
+            circuits.RemoveAll(c => c.Junctions.Count == 0);
 
-            if (++processedCount == 1000)
-                part1 = GetProduct(circuits);
+            if (--partOneConnectionCount == 0)
+            {
+                part1 = circuits
+                    .Select(x => x.Junctions.Count)
+                    .OrderDescending()
+                    .Take(3)
+                    .Aggregate(1L, (current, next) => current * next);
+            }
             
             if (circuits.Count == 1)
             {
-                part2 = Math.BigMul(p1.Position.X, p2.Position.X);
+                part2 = p1.Position.X * p2.Position.X;
                 break;
             }
         }
@@ -69,43 +72,34 @@ public sealed class Day08() : AdventDay(2025, 8)
         return (part1, part2);
     }
     
-    public static IEnumerable<JunctionBox> ParseInput(string input) => InputHelper
+    public static JunctionBox[] ParseInput(string input) => InputHelper
         .GetLines(input)
         .Select(line =>
         {
             var parts = line.Split(',').Select(int.Parse).ToArray();
             return new JunctionBox(parts[0], parts[1], parts[2]);
-        });
-    
-    public static Circuit[] ProcessCircuits(JunctionBox[] positions, int steps)
+        })
+        .ToArray();
+}
+
+public static class Day08Extensions
+{
+    extension(Day08.JunctionBox[] positions)
     {
-        foreach (var (p1, p2) in EnumerateOrderedPairs(positions).Take(steps))
+        public IOrderedEnumerable<(Day08.JunctionBox Box1, Day08.JunctionBox Box2)> EnumerateOrderedPairs()
         {
-            p1.ConnectTo(p2);
+            return positions.EnumeratePairs().OrderBy(x => x.Box1.GetDistanceTo(x.Box2));
         }
-        
-        return positions.Select(x => x.Circuit).Distinct().ToArray();
-    }
 
-    public static long GetProduct(ICollection<Circuit> circuits, int take = 3)
-    {
-        var biggest = circuits.Select(x => x.Junctions.Count).OrderDescending().Take(take).ToList();
-        return biggest.Skip(1).Aggregate(biggest[0], (current, b) => current * b);
-    }
-
-    public static IOrderedEnumerable<(JunctionBox Box1, JunctionBox Box2)> EnumerateOrderedPairs(JunctionBox[] positions)
-    {
-        return EnumeratePairs(positions).OrderBy(x => x.Box1.GetDistanceTo(x.Box2));
-    }
-    
-    private static IEnumerable<(JunctionBox Box1, JunctionBox Box2)> EnumeratePairs(JunctionBox[] positions)
-    {
-        var pairs = new HashSet<(JunctionBox, JunctionBox)>();
-        
-        foreach (var (p1, p2) in positions.SelectMany(p1 => positions.Select(p2 => (p1, p2))))
+        private IEnumerable<(Day08.JunctionBox Box1, Day08.JunctionBox Box2)> EnumeratePairs()
         {
-            if (p1 != p2 && pairs.Add((p1, p2)) && pairs.Add((p2, p1)))
-                yield return (p1, p2);
+            var pairs = new HashSet<(Day08.JunctionBox, Day08.JunctionBox)>();
+        
+            foreach (var (p1, p2) in positions.SelectMany(p1 => positions.Select(p2 => (p1, p2))))
+            {
+                if (p1 != p2 && pairs.Add((p1, p2)) && pairs.Add((p2, p1)))
+                    yield return (p1, p2);
+            }
         }
     }
 }
--- tests/csharp/Y2025/Day08Tests.cs
+++ tests/csharp/Y2025/Day08Tests.cs
@@ -1,10 +1,11 @@
+using AdventOfCode.Y2025;
 using Day08 = AdventOfCode.Y2025.Day08;
 
 namespace AdventOfCode.Tests.Y2025;
 
 public sealed class Day08Tests
 {
-    private readonly IAdventDay _solver = SolverRegistry.Get(2025, 8);
+    private readonly Day08 _solver = new();
 
     private const string Sample = """
     162,817,812
@@ -32,10 +33,8 @@ public sealed class Day08Tests
     [Fact]
     public void Sample_matches_part_one_problem_statement()
     {
-        var junctionBoxes = Day08.ParseInput(Sample).ToArray();
-        var circuits = Day08.ProcessCircuits(junctionBoxes, 10);
-        var product = Day08.GetProduct(circuits);
-        Assert.Equal(40, product);
+        var (p1, _) = Day08.Solve(Sample, 10);
+        Assert.Equal("40", p1);
     }
 
     [Fact]
@@ -48,43 +47,18 @@ public sealed class Day08Tests
     [Fact]
     public void Test_sort_order()
     {
-        var junctionBoxes = Day08.ParseInput(Sample).ToArray();
-        var ordered = Day08.EnumerateOrderedPairs(junctionBoxes).Take(5).ToList();
+        var ordered = Day08.ParseInput(Sample).EnumerateOrderedPairs().Take(4).ToList();
         
-        Assert.Equal("<162, 817, 812>", ordered[0].Box1.GetVector3().ToString());
-        Assert.Equal("<425, 690, 689>", ordered[0].Box2.GetVector3().ToString());
+        Assert.Equal((162, 817, 812), ordered[0].Box1.Position);
+        Assert.Equal((425, 690, 689), ordered[0].Box2.Position);
         
-        Assert.Equal("<162, 817, 812>", ordered[1].Box1.GetVector3().ToString());
-        Assert.Equal("<431, 825, 988>", ordered[1].Box2.GetVector3().ToString());
+        Assert.Equal((162, 817, 812), ordered[1].Box1.Position);
+        Assert.Equal((431, 825, 988), ordered[1].Box2.Position);
         
-        Assert.Equal("<906, 360, 560>", ordered[2].Box1.GetVector3().ToString());
-        Assert.Equal("<805, 96, 715>", ordered[2].Box2.GetVector3().ToString());
+        Assert.Equal((906, 360, 560), ordered[2].Box1.Position);
+        Assert.Equal((805, 096, 715), ordered[2].Box2.Position);
         
-        Assert.Equal("<431, 825, 988>", ordered[3].Box1.GetVector3().ToString());
-        Assert.Equal("<425, 690, 689>", ordered[3].Box2.GetVector3().ToString());
-    }
-    
-    [Theory]
-    [InlineData(1, 19)]
-    [InlineData(2, 18)]
-    [InlineData(3, 17)]
-    [InlineData(10, 11)]
-    public void Test_connection_count(int steps, int expected)
-    {
-        var junctionBoxes = Day08.ParseInput(Sample).ToArray();
-        var circuits = Day08.ProcessCircuits(junctionBoxes, steps);
-        Assert.Equal(expected, circuits.Length);
-    }
-
-    [Theory]
-    [InlineData(5, 1)]
-    [InlineData(4, 1)]
-    [InlineData(2, 2)]
-    [InlineData(1, 7)]
-    public void Test_junction_count(int junctionCount, int expectedNumCircuits)
-    {
-        var junctionBoxes = Day08.ParseInput(Sample).ToArray();
-        var circuits = Day08.ProcessCircuits(junctionBoxes, 10);
-        Assert.Equal(expectedNumCircuits, circuits.Count(c => c.Junctions.Count == junctionCount));
+        Assert.Equal((431, 825, 988), ordered[3].Box1.Position);
+        Assert.Equal((425, 690, 689), ordered[3].Box2.Position);
     }
 }
