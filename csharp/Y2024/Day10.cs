dbcb6cb66e36b1c604396b5f2730dc50f9185fc5
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-07 21:47:52 -0500

    2024 day 10 c#

---
+++ csharp/Y2024/Day10.cs
@@ -0,0 +1,63 @@
+namespace AdventOfCode.Y2024;
+
+using Coord = (int Row, int Col);
+
+public sealed class Day10 : IAdventDay
+{
+    public int Year => 2024;
+    public int Day => 10;
+
+    public AdventDaySolution Solve(string input)
+    {
+        var lines = InputHelper.GetLines(input);
+        return (SolvePartOne(lines), SolvePartTwo(lines));
+    }
+
+    private static long SolvePartOne(string[] lines) => lines
+        .EnumerateStartingCoords()
+        .SelectMany(x => lines.EnumeratePathEnds(x).Distinct())
+        .Count();
+
+    private static long SolvePartTwo(string[] lines) => lines
+        .EnumerateStartingCoords()
+        .SelectMany(lines.EnumeratePathEnds)
+        .Count();
+}
+
+file static class Extensions
+{
+    public static IEnumerable<Coord> EnumerateStartingCoords(this string[] lines)
+    {
+        for (var r = 0; r < lines.Length; r++)
+        for (var c = 0; c < lines[r].Length; c++)
+            if (lines[r][c] == '0')
+                yield return new Coord(r, c);
+    }
+    
+    public static IEnumerable<Coord> EnumeratePathEnds(this string[] lines, Coord pos)
+    {
+        if (lines[pos.Row][pos.Col] == '9')
+            yield return pos;
+
+        foreach (var next in EnumerateNextSteps(lines, pos))
+        foreach (var nextEnd in EnumeratePathEnds(lines, next))
+            yield return nextEnd;
+    }
+
+    public static IEnumerable<Coord> EnumerateNextSteps(string[] map, Coord pos)
+    {
+        var nextValue = map[pos.Row][pos.Col] + 1;
+        
+        if (pos.Row > 0 && map[pos.Row - 1][pos.Col] == nextValue)
+            yield return new Coord(pos.Row - 1, pos.Col);
+        
+        if (pos.Row < map.Length - 1 && map[pos.Row + 1][pos.Col] == nextValue)
+            yield return new Coord(pos.Row + 1, pos.Col);
+
+        if (pos.Col > 0 && map[pos.Row][pos.Col - 1] == nextValue)
+            yield return new Coord(pos.Row, pos.Col - 1);
+
+        if (pos.Col < map[0].Length - 1 && map[pos.Row][pos.Col + 1] == nextValue)
+            yield return new Coord(pos.Row, pos.Col + 1);
+    }
+}
---
+++ tests/csharp/Y2024/Day10Tests.cs
@@ -0,0 +1,31 @@
+namespace AdventOfCode.Tests.Y2024;
+
+public class Day10Tests
+{
+    private readonly IAdventDay _solver = SolverRegistry.Get(2024, 10);
+
+    private const string Sample = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """;
+
+    [Fact]
+    public void Sample_matches_part_one_problem_statement()
+    {
+        var (p1, _) = _solver.Solve(Sample);
+        Assert.Equal("36", p1);
+    }
+
+    [Fact]
+    public void Sample_matches_part_two_problem_statement()
+    {
+        var (_, p2) = _solver.Solve(Sample);
+        Assert.Equal("81", p2);
+    }
+}
