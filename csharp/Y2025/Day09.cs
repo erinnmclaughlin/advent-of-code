05e6c9592b45b894445879e0bcf4321b4b9934d5
Erin McLaughlin <sa-em5mb@myuva.onmicrosoft.com>
2025-12-09 18:18:55 -0500

    omg

--- csharp/Y2025/Day09.cs
+++ csharp/Y2025/Day09.cs
@@ -1,14 +1,45 @@
+using System.Collections.Immutable;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Y2025;
 
+// 94006416 is too low
+// 1963592680 is too high
 public sealed class Day09() : AdventDay(2025, 9)
 {
     public override AdventDaySolution Solve(string input)
     {
         var cells = ParseInput(input);
         var maxArea = cells.Max(c1 => cells.Where(c2 => c2 != c1).Max(c2 => GetArea(c1, c2)));
-        return (maxArea, "");
+        return (maxArea, SolvePartTwo(input));
+    }
+    
+    public static long SolvePartTwo(string input)
+    {
+        var cells = ParseInput(input);
+        var (rows, cols) = EnumerateEdges(cells);
+        var shape = new GridShape(rows, cols);
+
+        var maxContainedArea = 0L;
+
+        foreach (var c1 in cells)
+        {
+            foreach (var c2 in cells.Where(c2 => c1 != c2))
+            {
+                var area = GetArea(c1, c2);
+
+                if (maxContainedArea >= area)
+                    continue;
+
+                var subShape = GetShape(c1, c2);
+                if (shape.Contains(subShape))
+                {
+                    maxContainedArea = area;
+                }
+            }
+        }
+        
+        return maxContainedArea;
     }
 
     public static HashSet<GridCell> ParseInput(string input)
@@ -23,45 +54,88 @@ public sealed class Day09() : AdventDay(2025, 9)
         return width * height;
     }
 
-    public static IEnumerable<IGridEdge> EnumerateEdges(HashSet<GridCell> cells)
+    public static (HashSet<GridRow>, HashSet<GridCol>) EnumerateEdges(HashSet<GridCell> cells)
     {
         if (cells.Count < 2)
             throw new ArgumentException("Must have at least two grid cells.", nameof(cells));
 
+        var rows = new HashSet<GridRow>();
+        var cols = new HashSet<GridCol>();
+
         for (var i = 1; i < cells.Count; i++)
         {
-            yield return GetEdge(cells.ElementAt(i), cells.ElementAt(i - 1));
+            var c1 = cells.ElementAt(i);
+            var c2 = cells.ElementAt(i - 1);
+
+            if (c1.Row == c2.Row)
+                rows.Add(GetRow(c1, c2));
+            else
+                cols.Add(GetCol(c1, c2));
         }
 
-        yield return GetEdge(cells.Last(), cells.First());
+        var first = cells.First();
+        var last = cells.Last();
+        if (first.Row == last.Row)
+            rows.Add(GetRow(first, last));
+        else
+            cols.Add(GetCol(first, last));
+
+        return (rows, cols);
     }
 
-    public static IGridEdge GetEdge(GridCell currCell, GridCell prevCell)
+    public static GridRow GetRow(GridCell cell1, GridCell cell2)
     {
-        if (currCell.Col == prevCell.Col)
-        {
-            var startRow = Math.Min(currCell.Row, prevCell.Row);
-            var endRow = Math.Max(currCell.Row, prevCell.Row);
-
-            var size = endRow - startRow + 1;
-            return new GridCol(new GridCell(currCell.Col, startRow), size);
-        }
+        if (cell1.Row != cell2.Row)
+            throw new InvalidOperationException();
         
-        if (currCell.Row == prevCell.Row)
-        {
-            var startCol = Math.Min(currCell.Col, prevCell.Col);
-            var endCol = Math.Max(currCell.Col, prevCell.Col);
-            var size = endCol - startCol + 1;
-            return new GridRow(new GridCell(startCol, currCell.Row), size);
-        }
+        var start = cell1.Col < cell2.Col ? cell1 : cell2;
+        return new GridRow(start, GetWidth(cell1, cell2));
+    }
 
-        throw new InvalidOperationException();
+    public static GridCol GetCol(GridCell cell1, GridCell cell2)
+    {
+        if (cell1.Col != cell2.Col)
+            throw new InvalidOperationException();
+        
+        var start = cell1.Row < cell2.Row ? cell1 : cell2;
+        return new GridCol(start, GetHeight(cell1, cell2));
+    }
+    
+    public static GridRectangle GetShape(GridCell cell1, GridCell cell2)
+    {
+        var (topLeft, bottomRight) = GetTopLeftBottomRight(cell1, cell2);
+        return new GridRectangle(topLeft, bottomRight);
+    }
+
+    public static int GetWidth(GridCell cell1, GridCell cell2)
+    {
+        return Math.Abs(cell1.Col - cell2.Col) + 1;
+    }
+
+    public static int GetHeight(GridCell cell1, GridCell cell2)
+    {
+        return Math.Abs(cell1.Row - cell2.Row) + 1;
+    }
+    
+    public static (GridCell TopLeft, GridCell BottomRight) GetTopLeftBottomRight(GridCell cell1, GridCell cell2)
+    {
+        var minRow = Math.Min(cell1.Row, cell2.Row);
+        var maxRow = Math.Max(cell1.Row, cell2.Row);
+        var minCol = Math.Min(cell1.Col, cell2.Col);
+        var maxCol = Math.Max(cell1.Col, cell2.Col);
+
+        var topLeft = new GridCell(Col: minCol, Row: minRow);
+        var bottomRight = new GridCell(Col: maxCol, Row: maxRow);
+
+        return (topLeft, bottomRight);
     }
 
     public interface IGridEdge
     {
+        GridCell Start { get; }
+        GridCell End { get; }
         bool Contains(GridCell cell);
-        bool Intersects(IGridEdge other);
+        IEnumerable<GridCell> EnumerateCells();
     }
 
     public sealed record GridCol(GridCell Start, int Size) : IGridEdge
@@ -75,9 +149,10 @@ public sealed class Day09() : AdventDay(2025, 9)
             return cell.Row >= Start.Row && cell.Row <= (Start.Row + Size - 1);
         }
 
-        public bool Intersects(IGridEdge other)
+        public bool Intersects(GridRow other)
         {
-            return EnumerateCells().Any(other.Contains);
+            var point = new GridCell(Start.Col, other.Start.Row);
+            return Contains(point) && other.Contains(point);
         }
 
         public IEnumerable<GridCell> EnumerateCells()
@@ -98,9 +173,10 @@ public sealed class Day09() : AdventDay(2025, 9)
             return cell.Col >= Start.Col && cell.Col <= (Start.Col + Size - 1);
         }
         
-        public bool Intersects(IGridEdge other)
+        public bool Intersects(GridCol other)
         {
-            return EnumerateCells().Any(other.Contains);
+            var point = new GridCell(other.Start.Col, Start.Row);
+            return Contains(point) && other.Contains(point);
         }
 
         public IEnumerable<GridCell> EnumerateCells()
@@ -116,6 +192,139 @@ public sealed class Day09() : AdventDay(2025, 9)
         {
             var parts = input.Split(',');
             return new GridCell(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+    }
+
+    public sealed class GridRectangle : GridShape
+    {
+        public int Top { get; }
+        public int Right { get; }
+        public int Bottom { get; }
+        public int Left { get; }
+
+        public GridRow TopRow => Rows.ElementAt(0);
+        public GridRow BottomRow => Rows.ElementAt(1);
+        public GridCol LeftCol => Cols.ElementAt(0);
+        public GridCol RightCol => Cols.ElementAt(1);
+        
+        public GridRectangle(GridCell topLeft, GridCell bottomRight) : base(GetRows(topLeft, bottomRight), GetCols(topLeft, bottomRight))
+        {
+            Top = topLeft.Row;
+            Right = bottomRight.Col;
+            Bottom = bottomRight.Row;
+            Left = topLeft.Col;
+        }
+
+        public override bool Contains(GridCell cell)
+        {
+            return cell.Row >= Top && cell.Row <= Bottom && cell.Col >= Left && cell.Col <= Right;
+        }
+
+        public override bool Contains(GridRow row)
+        {
+            return Contains(row.Start) && Contains(row.End);
+        }
+        
+        public override bool Contains(GridCol col)
+        {
+            return Contains(col.Start) && Contains(col.End);
+        }
+
+        private static HashSet<GridRow> GetRows(GridCell topLeft, GridCell bottomRight)
+        {
+            var width = GetWidth(topLeft, bottomRight);
+            var topRow = new GridRow(topLeft, width);
+            var bottomRow = new GridRow(new GridCell(Col: topLeft.Col, Row: bottomRight.Row), width);
+            return [topRow, bottomRow];
+        }
+
+        private static HashSet<GridCol> GetCols(GridCell topLeft, GridCell bottomRight)
+        {
+            var height = GetHeight(topLeft, bottomRight);
+            var leftCol = new GridCol(topLeft, height);
+            var rightCol = new GridCol(new GridCell(Col: bottomRight.Col, Row: topLeft.Row), height);
+            return [leftCol, rightCol];
+        }
+    }
+
+    public class GridShape
+    {
+        protected ImmutableHashSet<GridRow> Rows { get; }
+        protected ImmutableHashSet<GridCol> Cols { get; }
+
+        public GridShape(HashSet<GridRow> rows, HashSet<GridCol> cols)
+        {
+            Rows = rows.ToImmutableHashSet();
+            Cols = cols.ToImmutableHashSet();
+        }
+
+        public virtual bool Contains(GridShape shape)
+        {
+            return shape.Rows.All(Contains) && shape.Cols.All(Contains);
+        }
+
+        public virtual bool Contains(GridRow row)
+        {
+            if (!Contains(row.Start))
+                return false;
+
+            return !Cols.Any(c => c.Start.Row != row.Start.Row && c.End.Row != row.End.Row && c.Intersects(row));
+        }
+        
+        public virtual bool Contains(GridCol col)
+        {
+            if (!Contains(col.Start))
+                return false;
+
+            return !Rows.Any(r => r.Start.Col != col.Start.Col && r.End.Col != col.End.Col && r.Intersects(col));
+        }
+        
+        public virtual bool Contains(GridCell cell)
+        {
+            return HasRowAbove(cell) && HasRowBelow(cell) && HasColumnLeft(cell) && HasColumnRight(cell);
+        }
+
+        public IEnumerable<GridCell> EnumerateCells()
+        {
+            return Rows.SelectMany(row => row.EnumerateCells()).Concat(Cols.SelectMany(col => col.EnumerateCells()));
+        }
+
+        private bool HasRowAbove(GridCell cell)
+        {
+            return Rows.Any(r =>
+                // above
+                r.Start.Row <= cell.Row &&
+                // covers
+                r.Start.Col <= cell.Col && r.End.Col >= cell.Col);
+        }
+
+        private bool HasRowBelow(GridCell cell)
+        {
+            return Rows.Any(r =>
+                // below
+                r.Start.Row >= cell.Row &&
+                // covers
+                r.Start.Col <= cell.Col && r.End.Col >= cell.Col);
+        }
+
+        private bool HasColumnLeft(GridCell cell)
+        {
+            return Cols
+                .Any(c =>
+                    // left
+                    c.Start.Col <= cell.Col &&
+                    // covers
+                    c.Start.Row <= cell.Row && c.End.Row >= cell.Row);
+        }
+
+        private bool HasColumnRight(GridCell cell)
+        {
+            return Cols
+                .Any(c =>
+                    // right
+                    c.Start.Col >= cell.Col &&
+                    // covers
+                    c.Start.Row <= cell.Row && c.End.Row >= cell.Row);
         }
     }
 }
--- tests/csharp/Y2025/Day09Tests.cs
+++ tests/csharp/Y2025/Day09Tests.cs
@@ -28,8 +28,7 @@ public sealed class Day09Tests(ITestOutputHelper output)
     [Fact]
     public void Sample_matches_part_two_problem_statement()
     {
-        var (_, p2) = _solver.Solve(Sample);
-        Assert.Skip("Not implemented");
+        var p2 = Day09.SolvePartTwo(Sample).ToString();
         Assert.Equal("24", p2);
     }
     
@@ -50,7 +49,7 @@ public sealed class Day09Tests(ITestOutputHelper output)
         var cell2 = new Day09.GridCell(Col: 7, Row: 3);
         var cell3 = new Day09.GridCell(Col: 11, Row: 1);
         
-        var col = Assert.IsType<Day09.GridCol>(Day09.GetEdge(cell1, cell2));
+        var col = Day09.GetCol(cell1, cell2);
         Assert.Equal(3, col.Size);
         Assert.True(col.Contains(cell1));
         Assert.True(col.Contains(cell2));
@@ -58,7 +57,7 @@ public sealed class Day09Tests(ITestOutputHelper output)
         Assert.False(col.Contains(new Day09.GridCell(Col: 7, Row: 4)));
 
         
-        var row = Assert.IsType<Day09.GridRow>(Day09.GetEdge(cell1, cell3));
+        var row = Day09.GetRow(cell1, cell3);
         Assert.Equal(5, row.Size);
         Assert.True(row.Contains(cell1));
         Assert.True(row.Contains(cell3));
@@ -88,10 +87,79 @@ public sealed class Day09Tests(ITestOutputHelper output)
     }
 
     [Fact]
+    public void GetFullShapeWorks1()
+    {
+        var cells = Day09.ParseInput(Sample);
+        var (rows, cols) = Day09.EnumerateEdges(cells);
+        var shape = new Day09.GridShape(rows, cols);
+        
+        var minCol = cells.Min(c => c.Col) - 2;
+        var maxCol = cells.Max(c => c.Col) + 2;
+        var minRow = cells.Min(c => c.Row) - 1;
+        var maxRow = cells.Max(c => c.Row) + 1;
+        
+        var sb = new StringBuilder();
+        for (var row = minRow; row <= maxRow; row++)
+        {
+            for (var col = minCol; col <= maxCol; col++)
+            {
+                var c = new Day09.GridCell(col, row);
+                
+                if (shape.Contains(c))
+                    sb.Append('X');
+                else
+                    sb.Append('.');
+            }
+            
+            sb.AppendLine();
+        }
+
+        var result = sb.ToString().TrimEnd();
+
+        output.WriteLine(result);
+    }
+    
+    [Fact]
+    public void GetFullShapeWorks2()
+    {
+        var cells = Day09.ParseInput(Sample);
+        var (rows, cols) = Day09.EnumerateEdges(cells);
+        var shape = new Day09.GridShape(rows, cols);
+        
+        var minCol = cells.Min(c => c.Col) - 2;
+        var maxCol = cells.Max(c => c.Col) + 2;
+        var minRow = cells.Min(c => c.Row) - 1;
+        var maxRow = cells.Max(c => c.Row) + 1;
+
+        var sb = new List<List<char>>();
+        for (var row = minRow; row <= maxRow; row++)
+        {
+            var sbRow = new List<char>();
+            
+            for (var col = minCol; col <= maxCol; col++)
+            {
+                sbRow.Add('.');
+            }
+            
+            sb.Add(sbRow);
+        }
+
+        foreach (var cell in shape.EnumerateCells())
+        {
+            sb[cell.Row][cell.Col] = 'X';
+        }
+
+        foreach (var row in sb)
+        {
+            output.WriteLine(string.Join("", row));
+        }
+    }
+
+    [Fact]
     public void EnumerateEdgesWorks()
     {
         var cells = Day09.ParseInput(Sample);
-        var edges = Day09.EnumerateEdges(cells).ToHashSet();
+        var (rows, cols) = Day09.EnumerateEdges(cells);
 
         var minCol = cells.Min(c => c.Col) - 2;
         var maxCol = cells.Max(c => c.Col) + 2;
@@ -106,8 +174,11 @@ public sealed class Day09Tests(ITestOutputHelper output)
                 var c = new Day09.GridCell(col, row);
 
                 if (cells.Contains(c))
+                {
+                    Assert.True(rows.Any(r1 => r1.Contains(c) || cols.Any(c1 => c1.Contains(c))));
                     sb.Append('#');
-                else if (edges.Any(e => e.Contains(c)))
+                }
+                else if (rows.Any(r1 => r1.Contains(c) || cols.Any(c1 => c1.Contains(c))))
                     sb.Append('X');
                 else
                     sb.Append('.');
@@ -143,10 +214,86 @@ public sealed class Day09Tests(ITestOutputHelper output)
         var cell2 = new Day09.GridCell(Col: 7, Row: 3);
         var cell3 = new Day09.GridCell(Col: 11, Row: 1);
         
-        var col = Assert.IsType<Day09.GridCol>(Day09.GetEdge(cell1, cell2));
-        var row = Assert.IsType<Day09.GridRow>(Day09.GetEdge(cell1, cell3));
+        var col = Assert.IsType<Day09.GridCol>(Day09.GetCol(cell1, cell2));
+        var row = Assert.IsType<Day09.GridRow>(Day09.GetRow(cell1, cell3));
         
         Assert.True(col.Intersects(row));
         Assert.True(row.Intersects(col));
+    }
+
+    [Fact]
+    public void GetShapeWorks()
+    {
+        var topLeft = new Day09.GridCell(Col: 7, Row: 1);
+        var bottomRight = new Day09.GridCell(Col: 11, Row: 7);
+        var topRight = new Day09.GridCell(Col: 11, Row: 1);
+        var bottomLeft = new Day09.GridCell(Col: 7, Row: 7);
+        
+        var shape = Day09.GetShape(topLeft, bottomRight);
+        
+        Assert.True(shape.Contains(topLeft));
+        Assert.True(shape.Contains(bottomRight));
+        Assert.True(shape.Contains(topRight));
+        Assert.True(shape.Contains(bottomLeft));
+        
+        Assert.False(shape.Contains(new Day09.GridCell(Col: topLeft.Col - 1, topLeft.Row)));
+        Assert.False(shape.Contains(new Day09.GridCell(Col: topLeft.Col, topLeft.Row - 1)));
+        
+        Assert.False(shape.Contains(new Day09.GridCell(Col: topRight.Col + 1, topLeft.Row)));
+        Assert.False(shape.Contains(new Day09.GridCell(Col: topRight.Col, topLeft.Row - 1)));
+        
+        Assert.False(shape.Contains(new Day09.GridCell(Col: bottomLeft.Col - 1, bottomLeft.Row)));
+        Assert.False(shape.Contains(new Day09.GridCell(Col: bottomLeft.Col, bottomLeft.Row + 1)));
+        
+        Assert.False(shape.Contains(new Day09.GridCell(Col: bottomRight.Col + 1, bottomRight.Row)));
+        Assert.False(shape.Contains(new Day09.GridCell(Col: bottomRight.Col, bottomRight.Row + 1)));
+    }
+
+    [Fact]
+    public void CanCreateRow()
+    {
+        var left = new Day09.GridCell(Col: 7, Row: 1);
+        var right = new Day09.GridCell(Col: 11, Row: 1);
+
+        var row = new Day09.GridRow(left, 5);
+        Assert.True(row.Contains(left));
+        Assert.True(row.Contains(right));
+        Assert.False(row.Contains(new Day09.GridCell(Col: 6, Row: 1)));
+        Assert.False(row.Contains(new Day09.GridCell(Col: 12, Row: 1)));
+        Assert.False(row.Contains(new Day09.GridCell(Col: 7, Row: 0)));
+        Assert.Equal(left, row.Start);
+        Assert.Equal(right, row.End);
+    }
+
+    [Fact]
+    public void CanCreateCol()
+    {
+        var top = new Day09.GridCell(Col: 7, Row: 1);
+        var bottom = new Day09.GridCell(Col: 7, Row: 7);
+
+        var col = new Day09.GridCol(top, 7);
+        Assert.True(col.Contains(top));
+        Assert.True(col.Contains(bottom));
+        Assert.False(col.Contains(new Day09.GridCell(Col: 7, Row: 0)));
+        Assert.False(col.Contains(new Day09.GridCell(Col: 7, Row: 8)));
+    }
+
+    [Fact]
+    public void GetTopLeftBottomRightWorks()
+    {
+        var topLeft = new Day09.GridCell(Col: 7, Row: 1);
+        var bottomRight = new Day09.GridCell(Col: 11, Row: 7);
+        var topRight = new Day09.GridCell(Col: 11, Row: 1);
+        var bottomLeft = new Day09.GridCell(Col: 7, Row: 7);
+        
+        var tlbr = Day09.GetTopLeftBottomRight(topLeft, bottomRight);
+
+        var (tl, br) = tlbr;
+        Assert.Equal(tl, topLeft);
+        Assert.Equal(br, bottomRight);
+        
+        Assert.Equivalent(tlbr, Day09.GetTopLeftBottomRight(bottomRight, topLeft));
+        Assert.Equivalent(tlbr, Day09.GetTopLeftBottomRight(topRight, bottomLeft));
+        Assert.Equivalent(tlbr, Day09.GetTopLeftBottomRight(bottomLeft, topRight));
     }
 }
