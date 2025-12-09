a440c67fdf80d3c846b4450f7c0acb57441a75a5
Erin McLaughlin <sa-em5mb@myuva.onmicrosoft.com>
2025-12-09 08:58:05 -0500

    get edge points

--- csharp/Y2025/Day09.cs
+++ csharp/Y2025/Day09.cs
@@ -1,24 +1,63 @@
+using System.Drawing;
+
 namespace AdventOfCode.Y2025;
 
 public sealed class Day09() : AdventDay(2025, 9)
 {
     public override AdventDaySolution Solve(string input)
     {
-        var points = InputHelper.GetLines(input).Select(ParseLine).ToArray();
+        var points = InputHelper.GetLines(input).Select(ParseLine).ToHashSet();
         var maxArea = points.Max(p1 => points.Where(p2 => p2 != p1).Max(p2 => GetArea(p1, p2)));
         return (maxArea, "");
     }
 
-    public static long GetArea((int X, int Y) p1, (int X, int Y) p2)
+    public static long GetArea(Point p1, Point p2)
     {
         var width = Math.Abs(p1.X - p2.X) + 1L;
         var height = Math.Abs(p1.Y - p2.Y) + 1L;
         return width * height;
     }
 
-    public static (int X, int Y) ParseLine(string line)
+    public static Point ParseLine(string line)
     {
         var parts = line.Split(',');
-        return (int.Parse(parts[0]), int.Parse(parts[1]));
+        return new Point(int.Parse(parts[0]), int.Parse(parts[1]));
+    }
+
+    public static IEnumerable<Point> EnumerateEdges(HashSet<Point> points)
+    {
+        for (var i = 1; i < points.Count; i++)
+        {
+            var currPoint = points.ElementAt(i);
+            var prevPoint = points.ElementAt(i - 1);
+
+            foreach (var point in EnumeratePointsBetween(currPoint, prevPoint))
+                yield return point;
+        }
+
+        foreach (var point in EnumeratePointsBetween(points.First(), points.Last()))
+        {
+            yield return point;
+        }
+    }
+    
+    public static IEnumerable<Point> EnumeratePointsBetween(Point p1, Point p2)
+    {
+        if (p1.X == p2.X)
+        {
+            var minY = Math.Min(p1.Y, p2.Y);
+            var maxY = Math.Max(p1.Y, p2.Y);
+
+            for (var y = minY; y <= maxY; y++)
+                yield return p1 with { Y = y };
+        }
+        else
+        {
+            var minX = Math.Min(p1.X, p2.X);
+            var maxX = Math.Max(p1.X, p2.X);
+                
+            for (var x = minX; x <= maxX; x++)
+                yield return p1 with { X = x };
+        }
     }
 }
--- tests/csharp/Y2025/Day09Tests.cs
+++ tests/csharp/Y2025/Day09Tests.cs
@@ -1,8 +1,10 @@
+using System.Drawing;
+using System.Text;
 using AdventOfCode.Y2025;
 
 namespace AdventOfCode.Tests.Y2025;
 
-public sealed class Day09Tests
+public sealed class Day09Tests(ITestOutputHelper output)
 {
     private readonly Day09 _solver = new();
 
@@ -40,5 +42,54 @@ public sealed class Day09Tests
         var point2 = Day09.ParseLine(p2);
         var area = Day09.GetArea(point1, point2);
         Assert.Equal(expected, area);
+    }
+
+    [Fact]
+    public void EnumerateEdgesWorks()
+    {
+        var points = InputHelper.GetLines(Sample).Select(Day09.ParseLine).ToHashSet();
+        var edgePoints = Day09.EnumerateEdges(points).ToHashSet();
+
+        var minX = points.Min(p => p.X) - 2;
+        var maxX = points.Max(p => p.X) + 2;
+        var minY = points.Min(p => p.Y) - 1;
+        var maxY = points.Max(p => p.Y) + 1;
+
+        var sb = new StringBuilder();
+        for (var y = minY; y <= maxY; y++)
+        {
+            for (var x = minX; x <= maxX; x++)
+            {
+                var p = new Point(x, y);
+
+                if (points.Contains(p))
+                    sb.Append('#');
+                else if (edgePoints.Contains(p))
+                    sb.Append('X');
+                else
+                    sb.Append('.');
+            }
+            
+            sb.AppendLine();
+        }
+
+        var result = sb.ToString().TrimEnd();
+
+        output.WriteLine(result);
+
+        const string expected =
+            """
+            ..............
+            .......#XXX#..
+            .......X...X..
+            ..#XXXX#...X..
+            ..X........X..
+            ..#XXXXXX#.X..
+            .........X.X..
+            .........#X#..
+            ..............
+            """;
+        
+        Assert.Equal(expected, result);
     }
 }
